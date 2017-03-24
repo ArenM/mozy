8a8c065cf14a0c36cf514132cccedc87bfa2f768
ArenM <aren@ctgaia.net>
2017-03-24 10:59:07 -0400

    work on trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,7 +4,7 @@ class TripsController < ApplicationController
   # GET /trips
   # GET /trips.json
   def index
-    @trips = Trip.all
+    @trips = Trip.where(:user => current_user)
   end
 
   # GET /trips/1
