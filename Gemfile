da8bc98c0f1398aeaa0af3129a5749c0d77fc870
ArenM <aren@ctgaia.net>
2017-03-24 14:07:06 -0400

    add geocodeing for profiles

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 gem 'leaflet-rails', github: 'axyjo/leaflet-rails'
+gem 'geocoder'
 
 gem 'devise'
 
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,10 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  geocoded_by :full_address
+  after_validation :geocode, if: ->(obj){ obj.full_address.present? }
+  
+  def full_address
+    [street_address, town, state, zipcode].compact.join(', ')
+  end
 end
