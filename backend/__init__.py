077449fed82d259297dce4fe802d9b9e3d073959
= <=>
2019-03-31 22:23:45 -0400

    Start building user managemant using flask_security and flask_mongodb

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
+from flask_security import Security, MongoEngineUserDatastore, UserMixin, RoleMixin, login_required
+from backend.db import db, User, Role
 import os
 
 def create_app():
@@ -6,12 +9,31 @@ def create_app():
 
     # app.config.from_pyfile('config.py')
 
+    app.config['SECRET_KEY'] = 'dev'
+    app.config['MONGODB_SETTINGS'] = {
+        'db': 'mozy',
+        'host': 'db',
+        # 'port': 2071,
+        'username': 'root',
+        'password': 'root'
+    }
+
+    # db = MongoEngine(app)
+    db.init_app(app)
+    user_datastore = MongoEngineUserDatastore(db, User, Role)
+    security = Security(app, user_datastore)
+
     try:
         os.makedirs(app.instance_path)
     except FileExistsError:
         pass
 
+    @app.before_first_request
+    def create_user():
+        user_datastore.create_user(email='rn@peacevolution.org', password='password')
+
     @app.route('/')
+    # @login_required
     def root():
         return "Hello World"
 
